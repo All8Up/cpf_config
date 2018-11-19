6be01ed3f81aade08963921bad7c72dcfa8ac94f
All8Up <all8up@gmail.com>
2018-11-19 11:41:01 -0600

    Testing out putting everything in one place.

---
+++ Setup.cmake
@@ -0,0 +1,6 @@
+include ("${CMAKE_CURRENT_LIST_DIR}/HunterGate.cmake")
+HunterGate(
+    URL "https://github.com/ruslo/hunter/archive/v0.23.53.tar.gz"
+    SHA1 "5d9feddb1ebeca27e32634954fe4de37cf36f97d"
+    FILEPATH "${CPF_CONFIG}"
+)
