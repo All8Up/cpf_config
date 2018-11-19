05719618af1d78ae9dd140de3b16ecb1e9464506
All8Up <all8up@gmail.com>
2018-11-19 10:42:29 -0600

    Initial config setup.

---
+++ Config.cmake
@@ -0,0 +1,32 @@
+hunter_config (cpf
+    URL "https://github.com/All8Up/cpf_core/archive/v0.0.74.tar.gz"
+    SHA1 e5603245278c9f5c230978980a212195c6f42d02
+)
+hunter_config (cpf_kpi
+    URL "https://github.com/All8Up/cpf_kpi/archive/v0.0.6.tar.gz"
+    SHA1 2848183a69b4e52b46def2a7113033cc9e7f6dc2
+)
+hunter_config (cpf_data
+    URL "https://github.com/All8Up/cpf_data/archive/v0.0.8.tar.gz"
+    SHA1 2801c556974d294b42a5e8ce4a63b59e0660b5f3
+)
+hunter_config (cpf_data_sqlite3
+    URL "https://github.com/All8Up/cpf_data_sqlite3/archive/v0.0.1.tar.gz"
+    SHA1 c352c1fe534d0b8b852a4490b9e39280bd2c5fbf
+)
+hunter_config (cpf_scripting_python
+    URL "https://github.com/All8Up/cpf_scripting_python/archive/v0.0.13.tar.gz"
+    SHA1 305d3a8928411d2f3408dfea2d7ed72577070862
+)
+hunter_config (cpf_concurrency
+    URL "https://github.com/All8Up/cpf_concurrency/archive/v0.0.1.tar.gz"
+    SHA1 3e3fca84cea1e0804d49064d341017403233c993
+)
+hunter_config (cpf_async
+    URL "https://github.com/All8Up/cpf_async/archive/v0.0.11.tar.gz"
+    SHA1 deef234760445393489b81939cd605b00e414b6c
+)
+hunter_config (cpf_async_UV
+    URL "https://github.com/All8Up/cpf_async_UV/archive/v0.0.1.tar.gz"
+    SHA1 3bdf9128674e3832f5008901796e6841a6da90f5
+)
