5fa793045e8069509c6db58d6844c09fc29aab5c
All8Up <all8up@gmail.com>
2018-11-21 09:09:35 -0600

    Updated core and added passthrough config versioning.

--- Config.cmake
+++ Config.cmake
@@ -1,36 +1,63 @@
 hunter_config (cpf
-    URL "https://github.com/All8Up/cpf_core/archive/v0.0.75.tar.gz"
-    SHA1 4149ba09c642f57a13b8c1f501528db6bf8d4128
+    URL "https://github.com/All8Up/cpf_core/archive/v0.0.76.tar.gz"
+    SHA1 8090903f4875d07ffb4357aa390d1070accb0453
+    CMAKE_ARGS
+        CPF_CONFIG_VERSION=${CPF_CONFIG_VERSION}
+        CPF_TOOLCHAINS_VERSION=${CPF_TOOLCHAINS_VERSION}
 )
 hunter_config (cpf_kpi
     URL "https://github.com/All8Up/cpf_kpi/archive/v0.0.6.tar.gz"
     SHA1 2848183a69b4e52b46def2a7113033cc9e7f6dc2
+    CMAKE_ARGS
+        CPF_CONFIG_VERSION=${CPF_CONFIG_VERSION}
+        CPF_TOOLCHAINS_VERSION=${CPF_TOOLCHAINS_VERSION}
 )
 hunter_config (cpf_data
     URL "https://github.com/All8Up/cpf_data/archive/v0.0.8.tar.gz"
     SHA1 2801c556974d294b42a5e8ce4a63b59e0660b5f3
+    CMAKE_ARGS
+        CPF_CONFIG_VERSION=${CPF_CONFIG_VERSION}
+        CPF_TOOLCHAINS_VERSION=${CPF_TOOLCHAINS_VERSION}
 )
 hunter_config (cpf_data_sqlite3
     URL "https://github.com/All8Up/cpf_data_sqlite3/archive/v0.0.1.tar.gz"
     SHA1 c352c1fe534d0b8b852a4490b9e39280bd2c5fbf
+    CMAKE_ARGS
+        CPF_CONFIG_VERSION=${CPF_CONFIG_VERSION}
+        CPF_TOOLCHAINS_VERSION=${CPF_TOOLCHAINS_VERSION}
 )
 hunter_config (cpf_scripting_python
     URL "https://github.com/All8Up/cpf_scripting_python/archive/v0.0.13.tar.gz"
     SHA1 305d3a8928411d2f3408dfea2d7ed72577070862
+    CMAKE_ARGS
+        CPF_CONFIG_VERSION=${CPF_CONFIG_VERSION}
+        CPF_TOOLCHAINS_VERSION=${CPF_TOOLCHAINS_VERSION}
 )
 hunter_config (cpf_concurrency
     URL "https://github.com/All8Up/cpf_concurrency/archive/v0.0.1.tar.gz"
     SHA1 3e3fca84cea1e0804d49064d341017403233c993
+    CMAKE_ARGS
+        CPF_CONFIG_VERSION=${CPF_CONFIG_VERSION}
+        CPF_TOOLCHAINS_VERSION=${CPF_TOOLCHAINS_VERSION}
 )
 hunter_config (cpf_async
     URL "https://github.com/All8Up/cpf_async/archive/v0.0.13.tar.gz"
     SHA1 5aec3398d8eca1504575631869cdbe858795b07a
+    CMAKE_ARGS
+        CPF_CONFIG_VERSION=${CPF_CONFIG_VERSION}
+        CPF_TOOLCHAINS_VERSION=${CPF_TOOLCHAINS_VERSION}
 )
 hunter_config (cpf_async_UV
     URL "https://github.com/All8Up/cpf_async_UV/archive/v0.0.1.tar.gz"
     SHA1 3bdf9128674e3832f5008901796e6841a6da90f5
+    CMAKE_ARGS
+        CPF_CONFIG_VERSION=${CPF_CONFIG_VERSION}
+        CPF_TOOLCHAINS_VERSION=${CPF_TOOLCHAINS_VERSION}
 )
 hunter_config (cpf_scripting_python
     URL "https://github.com/All8Up/cpf_scripting_python/archive/v0.0.13.tar.gz"
     SHA1 305d3a8928411d2f3408dfea2d7ed72577070862
+    CMAKE_ARGS
+        CPF_CONFIG_VERSION=${CPF_CONFIG_VERSION}
+        CPF_TOOLCHAINS_VERSION=${CPF_TOOLCHAINS_VERSION}
 )
