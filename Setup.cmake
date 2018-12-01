f0a5b760c0939544c3caab7666f03102006751f7
All8Up <All8Up@gmail.com>
2018-11-30 21:50:51 -0600

    Corrected an issue with not actually setting up the cmake build shared libs flag.

--- Setup.cmake
+++ Setup.cmake
@@ -1,4 +1,9 @@
 option (CPF_BUILD_SHARED "Build libraries as shared." OFF)
+
+if (CPF_BUILD_SHARED)
+	set (BUILD_SHARED_LIBS ON CACHE STRING "Build shared libraries." FORCE)
+endif ()
+
 include ("${CMAKE_CURRENT_LIST_DIR}/HunterGate.cmake")
 HunterGate(
     URL "https://github.com/ruslo/hunter/archive/v0.23.53.tar.gz"
